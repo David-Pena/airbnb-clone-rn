05afd2755e114db02286565d00cf1a1752ae8d39
David Pena <dev.davidpe@gmail.com>
2024-03-31 15:03:28 -0700

    chore: listing item styled

--- components/Listings.tsx
+++ components/Listings.tsx
@@ -11,6 +11,7 @@ import React, { useEffect, useRef, useState } from "react";
 import { defaultStyles } from "@/constants/Styles";
 import { Link } from "expo-router";
 import { Ionicons } from "@expo/vector-icons";
+import Animated, { FadeInRight, FadeOutLeft } from "react-native-reanimated";
 
 interface Props {
   listings: any[];
@@ -32,7 +33,7 @@ const Listings = ({ listings: items, category }: Props) => {
   const renderRow: ListRenderItem<any> = ({ item }) => (
     <Link href={`/listing/${item.id}`} asChild>
       <TouchableOpacity>
-        <View style={styles.listing}>
+        <Animated.View style={styles.listing} entering={FadeInRight} exiting={FadeOutLeft}>
           <Image source={{ uri: item.medium_url }} style={styles.image} />
           <TouchableOpacity style={{ position: "absolute", right: 30, top: 30 }}>
             <Ionicons name="heart-outline" size={24} color={"#000"} />
@@ -57,7 +58,7 @@ const Listings = ({ listings: items, category }: Props) => {
             <Text style={{ fontFamily: "mon-sb" }}>€ {item.price}</Text>
             <Text style={{ fontFamily: "mon" }}>night</Text>
           </View>
-        </View>
+        </Animated.View>
       </TouchableOpacity>
     </Link>
   );
